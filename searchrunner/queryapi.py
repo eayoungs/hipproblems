8b67e42255b23c46522851f46caf37d41f56cb77
Eric Youngson <eayoungs@gmail.com>
2018-08-09 19:56:17 -0700

    Added dummy json to fail more tests ;-)

--- searchrunner/queryapi.py
+++ searchrunner/queryapi.py
@@ -1,13 +1,18 @@
 from __future__ import print_function
 from tornado import gen, web, ioloop
-
-import json
+from tornado.httpclient import AsyncHTTPClient
+from searchrunner.scrapers import get_scraper
 
 
 class FlightMetaSearchHandler(web.RequestHandler):
     @gen.coroutine
-    def get(self):
-        self.write(json.dumps({"results": [{"provider": "Expedia"}]}))
+    def get(self, *providers):
+        http_client = AsyncHTTPClient()
+        response = yield http_client.fetch(
+            'http://localhost:9000/scrapers/expedia')
+
+        # print(response.body)
+        self.write(response.body)
 
 
 ROUTES = [
