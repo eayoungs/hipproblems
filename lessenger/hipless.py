69f0623f893844280937784cc24dda6c72dde9d7
Eric Youngson <eayoungs@gmail.com>
2018-09-06 17:56:34 -0700

    Set specific origin

--- lessenger/hipless.py
+++ lessenger/hipless.py
@@ -7,7 +7,7 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def set_default_headers(self):
         print("setting headers!!!")
-        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Origin", "http://hipmunk.github.io")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', ' POST, DELETE, OPTIONS')
 
